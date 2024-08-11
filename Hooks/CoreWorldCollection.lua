f9f9ed7328b17f55547c01df571f6d23904fe63d
Jon Hansen-Blum <mako@makowo.dev>
2024-08-11 11:42:37 -0500

    Update CoreWorldCollection.lua

--- Hooks/CoreWorldCollection.lua
+++ Hooks/CoreWorldCollection.lua
@@ -1,9 +1,5 @@
 Hooks:PostHook(CoreWorldCollection, "on_world_loaded", "BLELoadTest", function(self, index)
-    if managers.worlddefinition and managers.worlddefinition._continent_definitions then
+    if managers.worlddefinition and managers.worlddefinition._continent_definitions and managers.editor then
         managers.editor:load_continents(managers.worlddefinition._continent_definitions)
     end
-    --local definition = self._world_definitions[index]
-    --self._mission_params[definition._world_id] = nil
-
-    --definition:init_done()
 end)
