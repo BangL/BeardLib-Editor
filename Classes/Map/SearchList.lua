e6501fc19e969c95262fed957e3e76ba3ef109aa
Oreztov <shabugi@outlook.com>
2025-08-06 18:32:00 +0300

    Fixed editor to main menu crash
    Fixed crash when you quit from a level in the editor to the main menu using the escape menu.

--- Classes/Map/SearchList.lua
+++ Classes/Map/SearchList.lua
@@ -1,20 +1,33 @@
 SearchList = SearchList or class(EditorPart)
 
 function SearchList:init(parent)
-    self._menu = parent._holder:pan("Tab", {visible = false, auto_height = false, stretch_to_bottom = true, scrollbar = false})
+    self._menu = parent._holder:pan("Tab",
+        { visible = false, auto_height = false, stretch_to_bottom = true, scrollbar = false })
 
     self._options = self._menu:group("Options")
-    local search = self._options:textbox("Search", ClassClbk(self, "do_search", false, false), nil, {textbox_offset = 22, control_slice = 0.75})
+    local search = self._options:textbox("Search", ClassClbk(self, "do_search", false, false), nil,
+        { textbox_offset = 22, control_slice = 0.75 })
     local h = self._options:ItemsHeight(1)
-    search:tb_imgbtn("ClearSearch", ClassClbk(self, "clear_search"), nil, BLE.Utils.EditorIcons.cross, {w = h, h = h, offset = 0, img_scale = 0.5, position = "RightCentery"})
+    search:tb_imgbtn("ClearSearch", ClassClbk(self, "clear_search"), nil, BLE.Utils.EditorIcons.cross,
+        { w = h, h = h, offset = 0, img_scale = 0.5, position = "RightCentery" })
 
     self._parent = parent
-    self._pages_panel = self._menu:holder("Pages", {align_method = "centered_grid", auto_align = false, h = 32, size = self._menu.size * 0.9, inherit_values = {offset = 2}, visible = not self.HIDE_PAGINATION})
+    self._pages_panel = self._menu:holder("Pages",
+        {
+            align_method = "centered_grid",
+            auto_align = false,
+            h = 32,
+            size = self._menu.size * 0.9,
+            inherit_values = { offset = 2 },
+            visible = not
+                self.HIDE_PAGINATION
+        })
 
     self._page = 1
     self._pages = 1
 
-    self._list = self._menu:pan("Units", {inherit_values = {size = self._menu.size * 0.8}, auto_align = false, auto_height = false, stretch_to_bottom = true})
+    self._list = self._menu:pan("Units",
+        { inherit_values = { size = self._menu.size * 0.8 }, auto_align = false, auto_height = false, stretch_to_bottom = true })
     self:do_search()
 end
 
@@ -32,8 +45,8 @@ end
 
 function SearchList:do_search(no_reset_page, no_clear, t)
     local item = self._options:GetItem("Search")
-    BeardLib:AddDelayedCall("BLEDoSearchList"..tostring(self), self._filtered == nil and 0 or 0.2, function()
-        if not alive(self._list) then -- CAN happen when the code reloads
+    BeardLib:AddDelayedCall("BLEDoSearchList" .. tostring(self), self._filtered == nil and 0 or 0.2, function()
+        if not managers.mission._missions then
             return
         end
         local search = item:Value():lower():split(",")
@@ -58,7 +71,7 @@ function SearchList:do_search(no_reset_page, no_clear, t)
 end
 
 function SearchList:sort_items()
-    table.sort(self._filtered, function(a,b)
+    table.sort(self._filtered, function(a, b)
         return a.name < b.name
     end)
 end
@@ -88,22 +101,25 @@ function SearchList:do_show(no_clear)
     local h = self._pages_panel:ItemsHeight(1)
     local icons = BLE.Utils.EditorIcons
 
-    local popt = {w = h, h = h, offset = 2, img_offset = 6}
+    local popt = { w = h, h = h, offset = 2, img_offset = 6 }
 
-    local back = self._pages_panel:tb_imgbtn("Backwards", ClassClbk(self, "set_page", self._page-1), nil, icons.arrow_left, popt)
+    local back = self._pages_panel:tb_imgbtn("Backwards", ClassClbk(self, "set_page", self._page - 1), nil,
+        icons.arrow_left, popt)
     back:SetEnabled(self._page ~= 1)
 
     for i, page_num in pairs(pagination) do
         if page_num == 1 or page_num == pages then
             local next_or_prev = pagination[i - 1] or pagination[i + 1]
-            if next_or_prev and math.abs(next_or_prev-page_num) > 1 then
-                self._pages_panel:lbl("...", {index = page_num == 1 and 3 or nil, w = 28, h = h, text_align = "center"})
+            if next_or_prev and math.abs(next_or_prev - page_num) > 1 then
+                self._pages_panel:lbl("...", { index = page_num == 1 and 3 or nil, w = 28, h = h, text_align = "center" })
             end
         end
-        self._pages_panel:button(page_num, ClassClbk(self, "set_page", page_num), {border_bottom = page_num == page, h = h, w = 32, text_align = "center"})
+        self._pages_panel:button(page_num, ClassClbk(self, "set_page", page_num),
+            { border_bottom = page_num == page, h = h, w = 32, text_align = "center" })
     end
 
-    self._pages_panel:tb_imgbtn("Forward", ClassClbk(self, "set_page", self._page+1), nil, icons.arrow_right, popt):SetEnabled(self._page ~= pages and pages > 1)
+    self._pages_panel:tb_imgbtn("Forward", ClassClbk(self, "set_page", self._page + 1), nil, icons.arrow_right, popt)
+        :SetEnabled(self._page ~= pages and pages > 1)
 
     if not no_clear then
         self._list:ClearItems()
@@ -124,14 +140,14 @@ function SearchList:do_show(no_clear)
 end
 
 function SearchList:get_page_ranges()
-    return self.PER_PAGE * (self._page-1), self.PER_PAGE * self._page
+    return self.PER_PAGE * (self._page - 1), self.PER_PAGE * self._page
 end
 
 function SearchList:create_list_item(item)
     return self._list:button(self:friendly_item_name(item), ClassClbk(self, "on_click_item", item), {
         border_left = true,
         border_color = self:get_border_color(item),
-        offset = {1, 4},
+        offset = { 1, 4 },
     })
 end
 
